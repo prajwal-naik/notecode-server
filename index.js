48b8eaaaa13ca721578a1d0ccda7aafe788f451c
prajwal-naik <naikprajwal40@gmail.com>
2025-02-28 21:09:31 -0700

    Change display port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/api/snippets", snippetsRouter);
 
 app.listen(3001, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3001');
 });
 
 modeule.exports = app;
